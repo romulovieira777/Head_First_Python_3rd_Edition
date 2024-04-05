bf1c79a6620567c5d4d181f9c3cc895b9e969a23
Romulo Vieira <romulo.vieira777@gmail.com>
2024-04-05 10:18:01 -0300

    Building A Webapp: Web Development

--- Chapter_07_Building_A_Webapp_Web_Development/Python_Code/swimclub.py
+++ Chapter_07_Building_A_Webapp_Web_Development/Python_Code/swimclub.py
@@ -1,35 +1,30 @@
 import statistics
 import Chapter_07_Building_A_Webapp_Web_Development.Python_Code.hfpy_utils as hfpy_utils
 
-FOLDER = "swimdata/"
 CHARTS = "charts/"
+FOLDER = "swimdata/"
 
 
 def read_swim_data(filename):
-    """
-    Return swim data from a file
+    """Return swim data from a file.
 
-    Give the name of a swimmer's file (in filename), extract all the required data, then return it to the caller as a
-    tuple.
+    Given the name of a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as a tuple.
     """
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
-
     with open(FOLDER + filename) as file:
         lines = file.readlines()
         times = lines[0].strip().split(",")
-
     converts = []
-
-    for time in times:
+    for t in times:
         # The minutes value might be missing, so guard against this causing a crash.
-        if ":" in time:
-            minutes, rest = time.split(':')
-            seconds, hundredths = rest.split('.')
+        if ":" in t:
+            minutes, rest = t.split(":")
+            seconds, hundredths = rest.split(".")
         else:
             minutes = 0
-            seconds, hundredths = time.split('.')
+            seconds, hundredths = t.split(".")
         converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths))
-
     average = statistics.mean(converts)
     mins_secs, hundredths = f"{(average / 100):.2f}".split(".")
     mins_secs = int(mins_secs)
@@ -40,13 +35,12 @@ def read_swim_data(filename):
     return swimmer, age, distance, stroke, times, average, converts  # Returned as a tuple.
 
 
-def produce_bar_chart(fn):
-    """
-    Given the name of a swimmer's file, produce a HTML/SVG-based bar chart.
+def produce_bar_chart(fn, location=CHARTS):
+    """Given the name of a swimmer's file, produce a HTML/SVG-based bar chart.
 
     Save the chart to the CHARTS folder. Return the path to the bar chart file.
     """
-    swimmer, age, distance, stroke, times, average, converts = swimclub.read_swim_data(fn)
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
     from_max = max(converts)
     times.reverse()
     converts.reverse()
@@ -59,22 +53,18 @@ def produce_bar_chart(fn):
                         <body>
                             <h3>{title}</h3>"""
     body = ""
-
     for n, t in enumerate(times):
         bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
         body = body + f"""
                             <svg height="30" width="400">
                                 <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
                             </svg>{t}<br />"""
-
     footer = f"""
                             <p>Average time: {average}</p>
                         </body>
                     </html>"""
-
     page = header + body + footer
-    save_to = f"{CHARTS}{fn.removesuffix('.txt')}.html"
-
+    save_to = f"{location}{fn.removesuffix('.txt')}.html"
     with open(save_to, "w") as sf:
         print(page, file=sf)
 
--- Chapter_07_Building_A_Webapp_Web_Development/app.py
+++ Chapter_07_Building_A_Webapp_Web_Development/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 
 import os
 import Chapter_07_Building_A_Webapp_Web_Development.Python_Code.swimclub as swimclub
@@ -9,13 +9,16 @@ app.secret_key = os.urandom(24)
 
 @app.get("/")
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        title="Welcome to the Swimclub system",
+    )
 
 
 def populate_data():
     if "swimmers" not in session:
         swim_files = os.listdir(swimclub.FOLDER)
-        # swim_files.remove(".DS_Store")
+        #swim_files.remove(".DS_Store")
         session["swimmers"] = {}
         for file in swim_files:
             name, *_ = swimclub.read_swim_data(file)
@@ -27,12 +30,33 @@ def populate_data():
 @app.get("/swimmers")
 def display_swimmers():
     populate_data()
-    return str(sorted(session["swimmers"]))
+    return render_template(
+        "select.html",
+        title="Select a swimmer",
+        url="/showfiles",
+        select_id="swimmer",
+        data=sorted(session["swimmers"]),
+    )
 
 
-@app.get("/files/<swimmer>")
-def get_swimmers_files(swimmer):
-    return str(session["swimmers"][swimmer])
+@app.post("/showfiles")
+def display_swimmers_files():
+    populate_data()
+    name = request.form["swimmer"]
+    return render_template(
+        "select.html",
+        title="Select an event",
+        url="/showbarchart",
+        select_id="file",
+        data=session["swimmers"][name],
+    )
+
+
+@app.post("/showbarchart")
+def show_bar_chart():
+    file_id = request.form["file"]
+    location = swimclub.produce_bar_chart(file_id, "templates/")
+    return render_template(location.split("/")[-1])
 
 
 if __name__ == "__main__":
